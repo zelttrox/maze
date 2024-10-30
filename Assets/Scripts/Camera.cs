2d43e869fd4090109921d3170ab0a08fb58b4fa5
zelttrox <dev.zeltrox@gmail.com>
2024-10-30 16:55:29 +0100

    Added Cam switch func

---
+++ Assets/Scripts/Camera.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+public class Camera : MonoBehaviour {
+
+    public GameObject Cam;
+
+    private bool isSwitched = false;
+
+    private Vector3 CamPosition1;
+    private Vector3 CamRotation1;
+
+    private Vector3 CamPosition2;
+    private Vector3 CamRotation2;
+
+    void Start() {
+        CamPosition1 = new Vector3(-8, 8, 0);
+        CamRotation1 = new Vector3(50, 90, 0);
+        CamPosition2 = new Vector3(0, 8, -8);
+        CamRotation2 = new Vector3(50, 0, 0);
+    }
+
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            SwitchView();
+        }
+    }
+
+    public void SwitchView() {
+        if (isSwitched) {
+            Cam.transform.position = CamPosition1;
+            Cam.transform.rotation = Quaternion.Euler(CamRotation1);
+        } else {
+            Cam.transform.position = CamPosition2;
+            Cam.transform.rotation = Quaternion.Euler(CamRotation2);
+        }
+        isSwitched = !isSwitched;
+    }
+}
